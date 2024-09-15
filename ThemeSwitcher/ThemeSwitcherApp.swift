1dd714a427c0549804767a076147cf1c265da05e
Oluwayomi Felix <77457879+Portia-dot@users.noreply.github.com>
2024-09-14 21:43:42 -0700

    Initial Commit

---
+++ ThemeSwitcher/ThemeSwitcherApp.swift
@@ -0,0 +1,17 @@
+//
+//  ThemeSwitcherApp.swift
+//  ThemeSwitcher
+//
+//  Created by Modamori Oluwayomi on 2024-09-14.
+//
+
+import SwiftUI
+
+@main
+struct ThemeSwitcherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
